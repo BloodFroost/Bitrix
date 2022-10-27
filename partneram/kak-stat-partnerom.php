55016a53293dc3f93563f55b1bbe380c4fdcfeda
BloodFroost <78503517+BloodFroost@users.noreply.github.com>
2022-10-27 06:18:25 +0300

    Add files via upload

---
+++ partneram/kak-stat-partnerom.php
@@ -0,0 +1,6 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("keywords", "мягкая мебель, офисная мебель, мебель для кухни, детская мебель");
+$APPLICATION->SetPageProperty("description", "Мебельная компания осуществляет производство мебели на высококлассном оборудовании с применением минимальной доли ручного труда, что позволяет обеспечить высокое качество нашей продукции.");
+$APPLICATION->SetTitle("Как стать партнером");
+?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
